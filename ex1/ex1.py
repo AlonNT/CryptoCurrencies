086838ea39b7a2cdcbbb99689c858171353b3e32
Daniel Afrimi <daniel.afrimi@mail.huji.ac.il>
2020-04-10 13:03:43 +0300

    some work

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -22,8 +22,12 @@ class Transaction:
 
     def get_txid(self) -> TxID:
         """Returns the identifier of this transaction. This is the sha256 of the transaction contents."""
-        # TODO what to return if the id is null?
-        return hashlib.sha256(self.input)
+        hash = hashlib.sha256()
+        hash.update(self.output)
+        hash.update(self.signature)
+        if self.input:
+            hash.update(self.input)
+        return TxID(hash.digest())
 
 
 class Block:
@@ -44,8 +48,9 @@ class Block:
 
         # Calculate block hash by passing all over the transaction id and hash them
         for tx in self.transactions_list:
-            hash.update(tx.input)
-        return BlockHash(hash)
+            if tx.input:
+                hash.update(tx.input)
+        return BlockHash(hash.digest())
 
     def get_transactions(self) -> List[Transaction]:
         """
@@ -61,14 +66,15 @@ class Block:
 class Bank:
 
     hash_to_blocks = {}
-    public_key_to_money = {}
+    utxo = list()
     mempool = None
     blockchain = None
 
     def __init__(self) -> None:
         """Creates a bank with an empty blockchain and an empty mempool."""
         self.mempool = list()
-        self.blockchain = list()
+        self.blockchain = GENESIS_BLOCK_PREV # BlockChain is set to the last block Hash in the chain
+
 
     def add_transaction_to_mempool(self, transaction: Transaction) -> bool:
         """
@@ -78,13 +84,22 @@ class Bank:
         (ii) the source doesn't have the coin that he tries to spend
         (iii) there is contradicting tx in the mempool.
         """
-        # Check if signature is valid
-        if ecdsa.VerifyingKey.from_der(transaction.output).verify(transaction.signature, transaction.input):
+        # Check if tx signature is valid
+        verified_transaction = None
+        for tx in self.utxo:
+            if tx.get_txid() == transaction.input:
+                verified_transaction = tx
+                break
+        try:
+            ecdsa.VerifyingKey.from_der(verified_transaction.output).verify(transaction.signature,  transaction.output + transaction.input)
+        except:
             return False
-        # source = self.public_key_to_money.get(transaction.)
 
-        # todo deals with the other cases
+        # todo deals with case 2 = maybe the above lines deal with it!??
 
+        for tx in self.mempool: # Case 3: contradicting tx, when we already have tx with the same tx input
+            if transaction.input == tx.input:
+                return False
 
         self.mempool.append(transaction)
         return True
@@ -102,17 +117,20 @@ class Bank:
         for transaction in self.mempool:
             if counter == 0:
                 break
-            transaction_list.append(transaction)
+            transaction_list.append(transaction) # Tx that will be in a new block
             counter -= 1
+
+        self.mempool= list() # Reset the mempool after taking the first 'limit' transactions
 
         pre_block_hash = self.get_latest_hash()
         new_block = Block(pre_block_hash, transaction_list)
 
-        self.update_source_money(transaction_list)
-        self.blockchain.append(new_block) # Add the new block to the block chain (todo needs to do it now?)
+        self.update_unspent_transtactions_after_block_creation(transaction_list) # Add/remove the new/old transactions
 
         hash_new_block = new_block.get_block_hash()
-        self.hash_to_blocks.update({hash_new_block, new_block}) # Add the new block with its hash to hash_to_block dict
+        self.hash_to_blocks.update({hash_new_block : new_block}) # Add the new block with its hash to hash_to_block dict
+
+        self.blockchain = hash_new_block # Make this block the last block in the blockchain
         return hash_new_block
 
 
@@ -121,19 +139,22 @@ class Bank:
         This function returns a block object given its hash. If the block doesnt exist, an exception is thrown..
         """
         block = self.hash_to_blocks.get(block_hash)
-        # TODO id the block hash is genesis blockHash what block should we return?
         if not block:
-            raise Exception() # todo Change to another exception type
+            raise Exception() # TODO Change to another exception type
         return block
 
     def get_latest_hash(self) -> BlockHash:
         """
         This function returns the last block hash the was created.
         """
-        last_block = self.blockchain[-1]
-        if not last_block:
-            return GENESIS_BLOCK_PREV # If there is not block yet, return the first genesis block
-        return last_block.get_block_hash()
+        # TODO dont delete yet
+        # if len(self.blockchain) == 0:
+        #     return GENESIS_BLOCK_PREV # If there is not block yet, return the first genesis block
+        #
+        # last_block = self.blockchain[-1]
+        # return last_block.get_block_hash()
+
+        return self.blockchain
 
     def get_mempool(self) -> List[Transaction]:
         """
@@ -145,7 +166,7 @@ class Bank:
         """
         This function returns the list of unspent transactions.
         """
-        raise NotImplementedError()
+        return self.utxo
 
     def create_money(self, target: PublicKey) -> None:
         """
@@ -154,17 +175,16 @@ class Bank:
         generate these random bytes using secrets.token_bytes(48).
         We assume only the bank calls this function (wallets will never call it).
         """
-        raise NotImplementedError()
+        random_signature = secrets.token_bytes(48)
+        new_transaction = Transaction(target, None, random_signature)
+        self.mempool.append(new_transaction)
 
-    def update_source_money(self, transactions_list):
-        for transaction in transactions_list:
-            source = transaction.output # Gets the public key of the receiver
-            if source in self.public_key_to_money:
-                num_coins = self.public_key_to_money[source]
-                self.public_key_to_money[source] = num_coins + 1
-            else:
-                self.public_key_to_money.update({source:1})
-
+    def update_unspent_transtactions_after_block_creation(self, new_transactions):
+        for tx in new_transactions:
+            for old_tx in self.utxo:
+                if tx.input == old_tx.get_txid():
+                    self.utxo.remove(old_tx)
+            self.utxo.append(tx)
 
 
 
@@ -173,8 +193,10 @@ class Wallet:
     Wallets keep track of the coins owned by them, and can create transactions to move these coins."""
 
     private_key = None
-    public_key = None  # todo maybe need to use the object PublicKey in the above file???
+    public_key = None
     number_coins = 0
+    unspent_transactions_id = set()
+    prev_block_updated = GENESIS_BLOCK_PREV
 
     def __init__(self) -> None:
         """
@@ -190,7 +212,10 @@ class Wallet:
         For this exercise, there is no need to validate all transactions in the block
         """
         # first build a list of blocks until our latest update.
-        raise NotImplementedError()
+        blocks_list = self.get_updated_blocks(bank)
+        for block in blocks_list:
+            self.analayze_tx(block)
+
 
     def create_transaction(self, target: PublicKey) -> Optional[Transaction]:
         """
@@ -199,9 +224,13 @@ class Wallet:
         If the wallet already spent a specific coin, then he should'nt spend it again until unfreeze_all() is called.
         The method returns None if there are no outputs that have not been spent already.
         """
-        # todo The method returns None if there are no outputs that have not been spent already.
-        # signature = self.private_key.sign(TxID)  # todo needs to sign the entire tx
-        # return Transaction(target, TxID, signature)
+        if len(self.unspent_transactions_id) == 0: # There is no unspent tx and we cant transfer money
+            return None
+
+        unspent_transaction = list(self.unspent_transactions_id)[0]
+        signature = self.private_key.sign(target + unspent_transaction) # TODO this is the way!!!!
+        return Transaction(target, unspent_transaction, signature)
+
 
     def unfreeze_all(self) -> None:
         """
@@ -224,6 +253,28 @@ class Wallet:
         This function returns the public address of this wallet in DER format (follow the code snippet in the pdf).
         """
         return self.public_key
+
+    def get_updated_blocks(self, bank: Bank):
+        blocks_list = list()
+        last_block_hash = bank.get_latest_hash()
+        while (last_block_hash != self.prev_block_updated):
+            block = bank.get_block(last_block_hash)
+            blocks_list.append(block)
+            last_block_hash = block.get_prev_block_hash()
+
+        self.prev_block_updated = bank.get_latest_hash()
+        return blocks_list
+
+    def analayze_tx(self, block: Block):
+        for tx in block.get_transactions():
+            if tx.output == self.public_key:
+                self.number_coins += 1
+                self.unspent_transactions_id.add(tx.get_txid())
+
+            if tx.input in self.unspent_transactions_id: #TODO check this - maybe to this before
+                self.number_coins -= 1
+                self.unspent_transactions_id.remove(tx.input)
+
 
 # importing this file should NOT execute code. It should only create definitions for the objects above.
 # Write any tests you have in a different file.
