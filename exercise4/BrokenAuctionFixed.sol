35410b2f679072b99bfb13e21ad349f6918a1574
Daniel Afrimi <daniel.afrimi@mail.huji.ac.il>
2020-05-31 13:39:00 +0300

    owner gets the money back when auction is terminated

--- exercise4/BrokenAuctionFixed.sol
+++ exercise4/BrokenAuctionFixed.sol
@@ -4,11 +4,17 @@ contract Auction {
     address payable private highestBidder;
     string highest_Bidder_name;
     uint private highestBid;
+    address payable private owner;
     mapping (address => uint) pendingWithdrawals;
+
+
+    constructor() public payable {
+        owner = msg.sender;
+    }
 
     function bid(string memory name) public payable {
         require(msg.value >= highestBid);
-        
+
         if (highestBidder != address(0)) {
             pendingWithdrawals[highestBidder] += msg.value;
             highest_Bidder_name = name;
@@ -17,27 +23,29 @@ contract Auction {
        highestBidder = msg.sender;
        highestBid = msg.value;
     }
-    
+
     function withdraw() public {
         uint amount = pendingWithdrawals[msg.sender];
         // Remember to zero the pending refund before sending to prevent re-entrancy attacks
         pendingWithdrawals[msg.sender] = 0;
         msg.sender.transfer(amount);
     }
-    
+
+    function terminate_auction() external payable {
+        require(msg.sender == owner);
+        owner.transfer(highestBid);
+    }
+
     function get_cur_winner() view external returns(address payable){
         return highestBidder;
     }
-    
-    function get_highest_bid() view external returns(uint){
+
+    function get_highest_bid() view external returns(uint) {
         return highestBid;
     }
-    
+
     function get_bidder_name() view external returns(string memory){
         return highest_Bidder_name;
     }
-    
-    function get_address_contract() view external returns(address){
-        return address(this);
-    }
+
 }
